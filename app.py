df97b1687d63c01f87d47341bf78ccbf3f830ced
hellosonu80 <hellosonu80@gmail.com>
2026-06-14 18:01:15 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+streamlit
+openai
+import streamlit as st
+from openai import OpenAI
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+st.title("AI Physics Tutor")
+
+question = st.text_input("Ask your Physics doubt:")
+
+if st.button("Get Answer"):
+    if question == "":
+        st.write("Please type a question.")
+    else:
+        response = client.responses.create(
+            model="gpt-5-mini",
+          input="""
+You are an expert JEE/NEET Physics teacher.
+Explain step by step in simple English.
+Use formulas only when needed.
+Give one small example.
+Student question:
+""" + question
+        )
+
+        st.write(response.output_text)
